ca6bbef4a6a63d0c57a45e36802d4de55a0aa398
Matthew Daddysman <mdaddysman@gmail.com>
2017-09-18 17:06:44 -0500

    Add Shiny App

---
+++ process_shiny_input.R
@@ -0,0 +1,33 @@
+library('reshape2')
+library('plyr')
+library('dplyr')
+
+#set the working directory using 'setwd()'
+
+filename <- "170427_3B11M_P13_Plate2a_Top"
+secperframe <- 0.1
+pixelsize <- 71 #nm
+
+msd <- read.csv(paste0("Output/",filename,"_msdfull_R.csv"), header = FALSE)
+ids <- read.csv(paste0("Output/",filename,"_msdids_R.csv"), header = TRUE)
+fits <- read.csv(paste0("Output/",filename,"_fit10_inter22__new_R.csv"), header = TRUE)
+
+pixelsizeum <- pixelsize/1000
+
+msd.m <- melt(msd, na.rm = TRUE, id.vars = 'V1')
+msd.m.rn <- msd.m %>% mutate(rownum = as.integer(substring(as.character(variable),2)))
+
+data1 <- left_join(msd.m.rn,ids,by = 'rownum') %>% select(ID = id, SizeClass = sizeclass, lagframe = V1, MSD = value, length) 
+data1$SizeClass <- factor(data1$SizeClass)
+data1$SizeClass <- revalue(data1$SizeClass,c("2"="L", "1"="S"))
+data1$SizeClass <- as.character(data1$SizeClass)
+
+fits$SizeClass <- as.character(fits$SizeClass)
+
+data <- left_join(data1,fits, by = c("ID","SizeClass")) %>% mutate(uniqueID = paste0(as.character(ID),SizeClass)) %>%
+  mutate(lagtime = lagframe*secperframe, MSDum = MSD*pixelsizeum^2, lengthtime = length*secperframe) %>% select(-Length,-Size,-NumSizeErrors)
+
+
+rm(list=setdiff(ls(), c("data","filename","pixelsize","secperframe")))
+v <- 0.1
+save.image(file = paste0("Output/",filename,"_shiny.Rdata"))
