9879a5988431de1fd9fdc8525c43e9608d141257
Matthew Daddysman <mdaddysman@gmail.com>
2017-09-20 12:03:12 -0500

    added close button

--- MSDPlotting/app.R
+++ MSDPlotting/app.R
@@ -22,69 +22,72 @@ msdrange <- c(-6,6)
 trajrange <- c(0.2,300)
 plotbreaks <- c(10^-6,10^-4,10^-2,10^0,10^2,10^4,10^6)
 plotlabels <- c(expression("10"^{"-6"}), expression("10"^{"-4"}), expression("10"^{"-2"}), expression("10"^{"0"}),
-               expression("10"^{"2"}), expression("10"^{"4"}), expression("10"^{"6"}))
+                expression("10"^{"2"}), expression("10"^{"4"}), expression("10"^{"6"}))
 mypalette <- brewer.pal(3,"Set1")
 deffrange <- c(floor(log10(min(data.numlength$D, na.rm = TRUE))), ceiling(log10(max(data.numlength$D, na.rm = TRUE))))
 
 theme.dist <- theme_bw() + theme(panel.grid.major.x = element_line(color = 'grey75', size = 0.3),
-                               panel.grid.minor.x = element_line(color = 'grey85', size = 0.25), 
-                               panel.grid.major.y = element_line(color = 'grey75', size = 0.3), 
-                               panel.grid.minor.y = element_line(color = 'grey85', size = 0.25), 
-                               panel.border = element_rect(fill = NA, color = 'black', size = 0.75),
-                               legend.key = element_blank())
+                                 panel.grid.minor.x = element_line(color = 'grey85', size = 0.25), 
+                                 panel.grid.major.y = element_line(color = 'grey75', size = 0.3), 
+                                 panel.grid.minor.y = element_line(color = 'grey85', size = 0.25), 
+                                 panel.border = element_rect(fill = NA, color = 'black', size = 0.75),
+                                 legend.key = element_blank())
 ctrl.width <- 3
 
 ui <- fluidPage(
   withMathJax(),
   fluidRow(
-           column(12,
-                  h3(paste0("Filename: ",filename))
-           )
+    column(11,
+           h3(paste0("Filename: ",filename))
+    ),
+    column(1,
+           actionButton("close", "Close")
+    )
   ),
   fluidRow(h6("Style Control")),
   fluidRow(
-           column(ctrl.width,
-                  sliderInput("fontsize_adjust", label = "Font Size",
-                              min = 8, max = 72, value = 24, step = 1, round = TRUE)
-           ),
-           column(ctrl.width,
-                  sliderInput("alpha_adjust", label = "Transparancy",
-                              min = 0, max = 1, value = 0.8, step = 0.01, round = -2)
-           ),
-           column(ctrl.width,
-                  sliderInput("line_adjust", label = "Line Width",
-                              min = 0.25, max = 5, value = 0.5, step = 0.25)
-           ),
-           column(ctrl.width,
-                  sliderInput("point_adjust", label = "Point Size",
-                              min = 0.25, max = 5, value = 0.5, step = 0.25)
-           )
+    column(ctrl.width,
+           sliderInput("fontsize_adjust", label = "Font Size",
+                       min = 8, max = 72, value = 24, step = 1, round = TRUE)
+    ),
+    column(ctrl.width,
+           sliderInput("alpha_adjust", label = "Transparancy",
+                       min = 0, max = 1, value = 0.8, step = 0.01, round = -2)
+    ),
+    column(ctrl.width,
+           sliderInput("line_adjust", label = "Line Width",
+                       min = 0.25, max = 5, value = 0.5, step = 0.25)
+    ),
+    column(ctrl.width,
+           sliderInput("point_adjust", label = "Point Size",
+                       min = 0.25, max = 5, value = 0.5, step = 0.25)
+    )
   ),
   fluidRow(h6("Data Display Control")),
   fluidRow(
-           column(ctrl.width,
-                  sliderInput("traj_length", label = "Trajectory Length [s]",
-                              min = trajrange[1], max = trajrange[2], value = 25, step = 0.1, round = -1)
-           ),
-           column(ctrl.width,
-                  selectInput("type", label = "Type:",
-                              choices = c("All","Granules","Scrums"))
-           ),
-           column(ctrl.width,
-                  sliderInput("delta_adjust", label = "\\( \\Delta \\) Range [s]",
-                              min = deltarange[1], max = deltarange[2], value = 10, step = 0.1, round = -1)
-           ),
-           column(ctrl.width,
-                  sliderInput("msd_adjust", label = "MSD Range [\\(\\mu m^2\\)] (Powers of 10)",
-                              min = msdrange[1], max = msdrange[2], value = c(-4,1), step = 0.1,
-                              round = -1)
-           )
+    column(ctrl.width,
+           sliderInput("traj_length", label = "Trajectory Length [s]",
+                       min = trajrange[1], max = trajrange[2], value = 25, step = 0.1, round = -1)
+    ),
+    column(ctrl.width,
+           selectInput("type", label = "Type:",
+                       choices = c("All","Granules","Scrums"))
+    ),
+    column(ctrl.width,
+           sliderInput("delta_adjust", label = "\\( \\Delta \\) Range [s]",
+                       min = deltarange[1], max = deltarange[2], value = 10, step = 0.1, round = -1)
+    ),
+    column(ctrl.width,
+           sliderInput("msd_adjust", label = "MSD Range [\\(\\mu m^2\\)] (Powers of 10)",
+                       min = msdrange[1], max = msdrange[2], value = c(-4,1), step = 0.1,
+                       round = -1)
+    )
   ),
   fluidRow(
-           column(7,
-                  h4(textOutput("numTraj1")),
-                  h4(textOutput("numTraj2"))
-           )
+    column(7,
+           h4(textOutput("numTraj1")),
+           h4(textOutput("numTraj2"))
+    )
   ),
   fluidRow(
     column(7,
@@ -103,10 +106,10 @@ ui <- fluidPage(
     )
   ),
   fluidRow(
-           column(width = 3, offset = 9,
-                  sliderInput("alpha_bin", label = "\\( \\alpha \\) Bin Size",
-                              min = 0.01, max = 0.5, value = 0.1, step = 0.01, round = -2)
-           )
+    column(width = 3, offset = 9,
+           sliderInput("alpha_bin", label = "\\( \\alpha \\) Bin Size",
+                       min = 0.01, max = 0.5, value = 0.1, step = 0.01, round = -2)
+    )
   ),
   fluidRow(
     column(7,
@@ -117,16 +120,23 @@ ui <- fluidPage(
     )
   ),
   fluidRow(
-           column(width = 3, offset = 9,
-                  sliderInput("deff_bin", label = "\\( D_{eff} \\) Bin Size",
-                              min = 0.01, max = 1, value = 0.1, step = 0.01, round = -2)
-           )
+    column(width = 3, offset = 9,
+           sliderInput("deff_bin", label = "\\( D_{eff} \\) Bin Size",
+                       min = 0.01, max = 1, value = 0.1, step = 0.01, round = -2)
+    )
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
+  observe({
+    if (input$close > 0) {
+      stopApp() 
+    }
+  })
+  
+  
   data.filter <- reactive(
     if(input$type == "Granules") {
       filter(data,lengthtime >= as.numeric(input$traj_length)) %>% filter(SizeClass == "S")
@@ -140,24 +150,24 @@ server <- function(input, output) {
   data.numlength.filter <- reactive(
     filter(data.numlength, lengthtime >= as.numeric(input$traj_length))
   )
-
+  
   output$numTraj1 <- renderText(
     paste0(
       "Displaying ", prettyNum(nrow(data.numlength.filter()), big.mark = ","), " out of ",
       prettyNum(nrow(data.numlength), big.mark = ","), " trajectories. [",
       round(nrow(data.numlength.filter())/nrow(data.numlength) * 100, digits = 2), "%]"
-      )
+    )
   )
   output$numTraj2 <- renderText(
     paste0("Granules: ", 
-      prettyNum(nrow(data.numlength.filter() %>% filter(SizeClass == "S")), big.mark = ","), " of ",
-      prettyNum(nrow(filter(data.numlength, SizeClass == "S")), big.mark = ","), " [",
-      round(nrow(data.numlength.filter() %>% filter(SizeClass == "S")) / nrow(filter(data.numlength, SizeClass == "S")) * 100, digits = 2),
-      "%]; Scrums: ",
-      prettyNum(nrow(data.numlength.filter() %>% filter(SizeClass == "L")), big.mark = ","), " of ",
-      prettyNum(nrow(filter(data.numlength, SizeClass == "L")), big.mark = ","), " [", 
-      round(nrow(data.numlength.filter() %>% filter(SizeClass == "L")) / nrow(filter(data.numlength, SizeClass == "L")) * 100, digits = 2),
-      "%]."
+           prettyNum(nrow(data.numlength.filter() %>% filter(SizeClass == "S")), big.mark = ","), " of ",
+           prettyNum(nrow(filter(data.numlength, SizeClass == "S")), big.mark = ","), " [",
+           round(nrow(data.numlength.filter() %>% filter(SizeClass == "S")) / nrow(filter(data.numlength, SizeClass == "S")) * 100, digits = 2),
+           "%]; Scrums: ",
+           prettyNum(nrow(data.numlength.filter() %>% filter(SizeClass == "L")), big.mark = ","), " of ",
+           prettyNum(nrow(filter(data.numlength, SizeClass == "L")), big.mark = ","), " [", 
+           round(nrow(data.numlength.filter() %>% filter(SizeClass == "L")) / nrow(filter(data.numlength, SizeClass == "L")) * 100, digits = 2),
+           "%]."
     )
   )
   
@@ -190,51 +200,51 @@ server <- function(input, output) {
       theme(text = element_text(size = as.numeric(input$fontsize_adjust)), legend.position = "bottom", legend.background = element_blank())
   )
   
-   output$msdPlot.alpha <- renderPlot(
-     ggplot(data.filter(),aes(x=lagtime, y=MSDum)) + 
-       geom_line(aes(color = alpha, group = uniqueID), alpha = as.numeric(input$alpha_adjust), size = as.numeric(input$line_adjust)) + 
-       geom_point(aes(color = alpha), size = as.numeric(input$point_adjust), alpha = as.numeric(input$alpha_adjust)) + 
-       scale_x_log10() + scale_y_log10(breaks = plotbreaks, labels = plotlabels) + 
-       coord_cartesian(xlim = c(0.1,as.numeric(input$delta_adjust)), ylim = 10^as.numeric(input$msd_adjust)) +
-       scale_color_gradient2(low = 'red', high = 'blue', mid = 'white', midpoint = 1, na.value = 'grey50', limits = c(0,2), 
-                             breaks = c(0,0.5,1,1.5,2), name = expression(alpha), guide = guide_colorbar(barwidth = 15, barheight = 1.5)) + 
-       theme_dark() + theme(text = element_text(size = as.numeric(input$fontsize_adjust)), legend.position = "bottom") + 
-       labs(x = expression(Delta~"[s]"), y = "MSD ["~mu~"m"^{"2"}~"]")
-   )
-   
-   output$msdPlot.Deff <- renderPlot(
-     ggplot(data.filter(),aes(x=lagtime, y=MSDum)) + 
-       geom_line(aes(color = D, group = uniqueID), alpha = as.numeric(input$alpha_adjust), size = as.numeric(input$line_adjust)) + 
-       geom_point(aes(color = D), size = as.numeric(input$point_adjust), alpha = as.numeric(input$alpha_adjust)) + 
-       scale_x_log10() + scale_y_log10(breaks = plotbreaks, labels = plotlabels) + 
-       coord_cartesian(xlim = c(0.1,as.numeric(input$delta_adjust)), ylim = 10^as.numeric(input$msd_adjust)) +
-       scale_color_gradient(trans = "log10", na.value = 'grey50', #limits = 10^deffrange, 
-                            name = expression("D"["eff"]~"["~mu~"m"^{"2"}~"/s]"), guide = guide_colorbar(barwidth = 15, barheight = 1.5)) + 
-       theme.dist + theme(text = element_text(size = as.numeric(input$fontsize_adjust)), legend.position = "bottom") + 
-       labs(x = expression(Delta~"[s]"), y = "MSD ["~mu~"m"^{"2"}~"]")
-   )
-   
-   output$Dist.alpha <- renderPlot(
-     ggplot(data.numlength.filter(),aes(x=alpha)) + 
-       geom_histogram(alpha = 0.5, color = 'black', position = 'identity', binwidth = as.numeric(input$alpha_bin), aes(y = ..density.., fill = SizeClass)) + 
-       geom_density(aes(color = SizeClass), size = 1+as.numeric(input$line_adjust)) +
-       scale_fill_brewer(palette = "Set1", labels = c("scrum", "granule"), name = NULL) + 
-       scale_color_brewer(palette = "Set1", guide = FALSE) + scale_x_continuous(limits = c(0,2)) +
-       labs(x = expression(alpha), y = "pdf") + theme.dist + 
-       theme(text = element_text(size = as.numeric(input$fontsize_adjust)), 
-             legend.position = "bottom", legend.background = element_blank())
-   )
-   
-   output$Dist.deff <- renderPlot(
-     ggplot(data.numlength.filter(),aes(x=D)) + 
-       geom_histogram(alpha = 0.5, color = 'black', position = 'identity', binwidth = as.numeric(input$deff_bin), aes(y = ..density.., fill = SizeClass)) + 
-       geom_density(aes(color = SizeClass), size = 1+as.numeric(input$line_adjust)) +
-       scale_fill_brewer(palette = "Set1", labels = c("scrum", "granule"), name = NULL) + 
-       scale_color_brewer(palette = "Set1", guide = FALSE) + scale_x_log10() +
-       labs(x = expression("D"["eff"]), y = "pdf") + theme.dist + 
-       theme(text = element_text(size = as.numeric(input$fontsize_adjust)), 
-             legend.position = "bottom", legend.background = element_blank())
-   )
+  output$msdPlot.alpha <- renderPlot(
+    ggplot(data.filter(),aes(x=lagtime, y=MSDum)) + 
+      geom_line(aes(color = alpha, group = uniqueID), alpha = as.numeric(input$alpha_adjust), size = as.numeric(input$line_adjust)) + 
+      geom_point(aes(color = alpha), size = as.numeric(input$point_adjust), alpha = as.numeric(input$alpha_adjust)) + 
+      scale_x_log10() + scale_y_log10(breaks = plotbreaks, labels = plotlabels) + 
+      coord_cartesian(xlim = c(0.1,as.numeric(input$delta_adjust)), ylim = 10^as.numeric(input$msd_adjust)) +
+      scale_color_gradient2(low = 'red', high = 'blue', mid = 'white', midpoint = 1, na.value = 'grey50', limits = c(0,2), 
+                            breaks = c(0,0.5,1,1.5,2), name = expression(alpha), guide = guide_colorbar(barwidth = 15, barheight = 1.5)) + 
+      theme_dark() + theme(text = element_text(size = as.numeric(input$fontsize_adjust)), legend.position = "bottom") + 
+      labs(x = expression(Delta~"[s]"), y = "MSD ["~mu~"m"^{"2"}~"]")
+  )
+  
+  output$msdPlot.Deff <- renderPlot(
+    ggplot(data.filter(),aes(x=lagtime, y=MSDum)) + 
+      geom_line(aes(color = D, group = uniqueID), alpha = as.numeric(input$alpha_adjust), size = as.numeric(input$line_adjust)) + 
+      geom_point(aes(color = D), size = as.numeric(input$point_adjust), alpha = as.numeric(input$alpha_adjust)) + 
+      scale_x_log10() + scale_y_log10(breaks = plotbreaks, labels = plotlabels) + 
+      coord_cartesian(xlim = c(0.1,as.numeric(input$delta_adjust)), ylim = 10^as.numeric(input$msd_adjust)) +
+      scale_color_gradient(trans = "log10", na.value = 'grey50', #limits = 10^deffrange, 
+                           name = expression("D"["eff"]~"["~mu~"m"^{"2"}~"/s]"), guide = guide_colorbar(barwidth = 15, barheight = 1.5)) + 
+      theme.dist + theme(text = element_text(size = as.numeric(input$fontsize_adjust)), legend.position = "bottom") + 
+      labs(x = expression(Delta~"[s]"), y = "MSD ["~mu~"m"^{"2"}~"]")
+  )
+  
+  output$Dist.alpha <- renderPlot(
+    ggplot(data.numlength.filter(),aes(x=alpha)) + 
+      geom_histogram(alpha = 0.5, color = 'black', position = 'identity', binwidth = as.numeric(input$alpha_bin), aes(y = ..density.., fill = SizeClass)) + 
+      geom_density(aes(color = SizeClass), size = 1+as.numeric(input$line_adjust)) +
+      scale_fill_brewer(palette = "Set1", labels = c("scrum", "granule"), name = NULL) + 
+      scale_color_brewer(palette = "Set1", guide = FALSE) + scale_x_continuous(limits = c(0,2)) +
+      labs(x = expression(alpha), y = "pdf") + theme.dist + 
+      theme(text = element_text(size = as.numeric(input$fontsize_adjust)), 
+            legend.position = "bottom", legend.background = element_blank())
+  )
+  
+  output$Dist.deff <- renderPlot(
+    ggplot(data.numlength.filter(),aes(x=D)) + 
+      geom_histogram(alpha = 0.5, color = 'black', position = 'identity', binwidth = as.numeric(input$deff_bin), aes(y = ..density.., fill = SizeClass)) + 
+      geom_density(aes(color = SizeClass), size = 1+as.numeric(input$line_adjust)) +
+      scale_fill_brewer(palette = "Set1", labels = c("scrum", "granule"), name = NULL) + 
+      scale_color_brewer(palette = "Set1", guide = FALSE) + scale_x_log10() +
+      labs(x = expression("D"["eff"]), y = "pdf") + theme.dist + 
+      theme(text = element_text(size = as.numeric(input$fontsize_adjust)), 
+            legend.position = "bottom", legend.background = element_blank())
+  )
 }
 
 
